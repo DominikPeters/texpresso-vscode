f095afc3a072667097f664487f061384fc90b299
Dominik Peters <mail@dominik-peters.de>
2024-05-03 18:07:54 +0200

    initial commit

---
+++ src/extension.ts
@@ -0,0 +1,213 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+// Import child_process
+import { ChildProcess, spawn, execSync } from 'child_process';
+// import tmp
+import * as tmp from 'tmp';
+// import fs
+import * as fs from 'fs';
+
+import Rope from './rope';
+
+const textEncoder = new TextEncoder();
+function byteLength(str : string) : number {
+  return textEncoder.encode(str).length;
+}
+
+tmp.setGracefulCleanup();
+
+let texpresso: ChildProcess;
+let rope: Rope;
+let filePath: string;
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	if (texpresso) {
+		texpresso.kill();
+	}
+
+	let activeEditor: vscode.TextEditor | undefined;
+
+	// The command has been defined in the package.json file
+	let disposable = vscode.commands.registerCommand('texpresso.startDocument', () => {
+		activeEditor = vscode.window.activeTextEditor;
+		if (activeEditor) {
+			// vscode.window.showInformationMessage('Starting Texpreso for this document');
+			filePath = activeEditor.document.fileName;
+			const text = activeEditor.document.getText();
+			rope = new Rope(text);
+			if (activeEditor.document.isUntitled) {
+				const tmpDir = tmp.dirSync();
+				filePath = tmpDir.name + '/untitled.tex';
+				fs.writeFileSync(filePath, text);
+				// on macOS, the temp file goes into a symlinked directory, so we need to resolve the path
+				filePath = fs.realpathSync(filePath);
+			}
+			console.log('Starting Texpresso for', filePath);
+			// Start texpresso
+			const command = vscode.workspace.getConfiguration('texpresso').get('command') as string;
+			// Check if command exists
+			try {
+				fs.accessSync(command, fs.constants.X_OK);
+			} catch (error) {
+				vscode.window.showErrorMessage(
+					`Texpresso command '${command}' does not exist or is not executable. Please check the 'texpresso.command' setting.`,
+					'Open Settings'
+				).then(value => {
+					if (value === 'Open Settings') {
+						vscode.commands.executeCommand('workbench.action.openSettings', 'texpresso.command');
+					}
+				});
+				return;
+			}
+			texpresso = spawn(command, ['-json', filePath]);
+			if (texpresso && texpresso.stdout) {
+				texpresso.stdout.on('data', data => {
+					const message = JSON.parse(data.toString());
+					if (message[0] === 'synctex' && message[1] === activeEditor?.document.fileName) {
+						const line = message[2];
+						activeEditor?.revealRange(new vscode.Range(line - 1, 0, line - 1, 0));
+					}
+				});
+			}
+			if (texpresso && texpresso.stderr) {
+				texpresso.stderr.on('data', data => {
+					console.error(`stderr: ${data}`);
+				});
+			}
+			// Send file content to texpresso
+			const message = ["open", filePath, activeEditor.document.getText()];
+			if (texpresso && texpresso.stdin) {
+				texpresso.stdin.write(JSON.stringify(message) + '\n');
+			}
+		}
+	});
+
+	vscode.workspace.onDidChangeTextDocument(event => {
+		if (activeEditor && event.document === activeEditor.document) {
+			// send change message to texpresso via stdin
+			texpresso?.stdin?.cork();
+			for (const change of event.contentChanges) {
+				// get byte offsets of change
+				const start = rope.byteOffset(change.rangeOffset);
+				const end = rope.byteOffset(change.rangeOffset + change.rangeLength);
+				// send change message
+				const message = ["change", filePath, start, end - start, change.text];
+				texpresso?.stdin?.write(JSON.stringify(message) + '\n');
+				// implement change in rope
+				rope.remove(change.rangeOffset, change.rangeOffset + change.rangeLength);
+				rope.insert(change.rangeOffset, change.text);
+				// const happy = rope.toString() === activeEditor.document.getText() && start == byteLength(activeEditor.document.getText().slice(0, change.rangeOffset));
+				// console.log('change-message', [change.rangeOffset, change.rangeLength], message.slice(2, 5), happy, change.range.start.character, change.range.end.character);
+			}
+			texpresso?.stdin?.uncork();
+		}
+	}, null, context.subscriptions);
+
+	let previouslySentLineNumber: number | undefined;
+	vscode.window.onDidChangeTextEditorSelection(event => {
+		if (activeEditor && event.textEditor.document === activeEditor.document) {
+			const lineNumber = activeEditor.selection.active.line;
+			if (!previouslySentLineNumber || previouslySentLineNumber !== lineNumber) {
+				previouslySentLineNumber = lineNumber;
+				if (texpresso && texpresso.stdin) {
+					const message = ["synctex-forward", filePath, lineNumber];
+					texpresso.stdin.write(JSON.stringify(message) + '\n');
+				}
+			}
+		}
+	});
+
+	context.subscriptions.push(disposable);
+
+	context.subscriptions.push(vscode.commands.registerCommand('texpresso.freshCompile', () => {
+		if (activeEditor) {
+			const text = activeEditor.document.getText();
+			rope = new Rope(text);
+			// resend "open" command
+			const message = ["open", filePath, text];
+			texpresso?.stdin?.write(JSON.stringify(message) + '\n');
+			texpresso?.stdin?.write(JSON.stringify(["rescan"]) + '\n');
+		}
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('texpresso.showPreview', () => {
+		if (true || activeEditor) {
+			const panel = vscode.window.createWebviewPanel(
+				'texpressoPreview',
+				'Texpresso Preview',
+				vscode.ViewColumn.Beside,
+				{ enableScripts: true }
+			);
+			panel.webview.html = `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Cat Coding</title>
+				<style>
+					body {
+						height: 100vh;
+					}
+				</style>
+			</head>
+			<body>
+				<script>
+
+					const vscode = acquireVsCodeApi();
+
+					const body = document.getElementsByTagName('body')[0];
+
+					function sendSizeMessage() {
+						// send resize message to texpresso
+						const message = {
+							type: 'resize',
+							windowWidth: window.screen.width,
+							windowHeight: window.screen.height,
+							windowTop: window.screenTop,
+							bodyWidth: body.offsetWidth,
+							bodyHeight: body.offsetHeight
+						}
+						vscode.postMessage(message);
+					}
+					
+					window.addEventListener('resize', sendSizeMessage);
+					sendSizeMessage();
+
+					setInterval(() => {
+						sendSizeMessage();
+					}, 1000);
+				</script>
+			</body>
+			</html>`;
+			panel.webview.onDidReceiveMessage(message => {
+				if (texpresso && texpresso.stdin) {
+					const texpressoMessage = [
+						"move-window", 
+						message.windowWidth - message.bodyWidth + 10, // space to see the resize handle
+						message.windowTop,
+						message.bodyWidth - 10,
+						message.windowHeight - message.windowTop
+					];
+					texpresso.stdin.write(JSON.stringify(texpressoMessage) + '\n');
+					// stay on top
+					const alwaysOnTopMessage = [
+						"stay-on-top",
+						"t"
+					];
+					texpresso.stdin.write(JSON.stringify(alwaysOnTopMessage) + '\n');
+				}
+			});
+		}
+	}));
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {
+	if (texpresso) {
+		texpresso.kill();
+	}
+}
---
+++ src/rope.ts
@@ -0,0 +1,238 @@
+// This code derived from https://github.com/component/rope
+
+// The MIT License (MIT)
+
+// Copyright (c) 2014 Automattic, Inc.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+const SPLIT_LENGTH = 1000;
+const JOIN_LENGTH = 500;
+const REBALANCE_RATIO = 1.2;
+
+const textEncoder = new TextEncoder();
+function byteLength(str : string) : number {
+  return textEncoder.encode(str).length;
+}
+
+/**
+ * Creates a rope data structure
+ *
+ * @param {String} str - String to populate the rope.
+ * @api public
+ */
+
+// function Rope(str) {
+//   // allow usage without `new`
+//   if (!(this instanceof Rope)) return new Rope(str);
+
+//   this._value = str;
+//   this.length = str.length;
+//   this.bytes = byteLength(str);
+//   adjust.call(this);
+// }
+
+class Rope {
+  private _value?: string;
+  public length: number;
+  public bytes: number;
+  private _left?: Rope;
+  private _right?: Rope;
+
+  constructor(str: string) {
+    this._value = str;
+    this.length = str.length;
+    this.bytes = byteLength(str);
+    this.adjust();
+  }
+
+  private adjust() {
+    if (typeof this._value != 'undefined') {
+      if (this.length > SPLIT_LENGTH) {
+        const divide = Math.floor(this.length / 2);
+        this._left = new Rope(this._value.substring(0, divide));
+        this._right = new Rope(this._value.substring(divide));
+        delete this._value;
+      }
+    } else if (this._left && this._right) {
+      if (this.length < JOIN_LENGTH) {
+        this._value = this._left.toString() + this._right.toString();
+        delete this._left;
+        delete this._right;
+      }
+    }
+  }
+
+  public toString(): string {
+    if (typeof this._value != 'undefined') {
+      return this._value;
+    } else if (this._left && this._right) {
+      return this._left.toString() + this._right.toString();
+    }
+    return '';
+  }
+
+  public remove(start: number, end: number): void {
+    if (start < 0 || start > this.length) throw new RangeError('Start is not within rope bounds.');
+    if (end < 0 || end > this.length) throw new RangeError('End is not within rope bounds.');
+    if (start > end) throw new RangeError('Start is greater than end.');
+    if (typeof this._value != 'undefined') {
+      this._value = this._value.substring(0, start) + this._value.substring(end);
+      this.length = this._value.length;
+      this.bytes = byteLength(this._value);
+    } else if (this._left && this._right) {
+      const leftLength = this._left.length;
+      const leftStart = Math.min(start, leftLength);
+      const leftEnd = Math.min(end, leftLength);
+      const rightLength = this._right.length;
+      const rightStart = Math.max(0, Math.min(start - leftLength, rightLength));
+      const rightEnd = Math.max(0, Math.min(end - leftLength, rightLength));
+      if (leftStart < leftLength) {
+        this._left.remove(leftStart, leftEnd);
+      }
+      if (rightEnd > 0) {
+        this._right.remove(rightStart, rightEnd);
+      }
+      this.length = this._left.length + this._right.length;
+      this.bytes = this._left.bytes + this._right.bytes;
+    }
+    this.adjust();
+  }
+
+  public insert(position: number, value: string): void {
+    if (position < 0 || position > this.length) throw new RangeError('position is not within rope bounds.');
+    if (typeof this._value != 'undefined') {
+      this._value = this._value.substring(0, position) + value.toString() + this._value.substring(position);
+    } else if (this._left && this._right) {
+      const leftLength = this._left.length;
+      if (position < leftLength) {
+        this._left.insert(position, value);
+      } else {
+        this._right.insert(position - leftLength, value);
+      }
+    }
+    this.length += value.length;
+    this.bytes += byteLength(value);
+    this.adjust();
+  }
+
+  public rebuild(): void {
+    if (this._left && this._right) {
+      this._value = this._left.toString() + this._right.toString();
+      delete this._left;
+      delete this._right;
+      this.adjust();
+    }
+  }
+
+  public rebalance(): void {
+    if (this._left && this._right) {
+      if (this._left.length / this._right.length > REBALANCE_RATIO ||
+          this._right.length / this._left.length > REBALANCE_RATIO) {
+        this.rebuild();
+      } else {
+        this._left.rebalance();
+        this._right.rebalance();
+      }
+    }
+  }
+
+  public substring(start: number, end?: number): string {
+    if (typeof end == 'undefined') {
+      end = this.length;
+    }
+    if (start < 0 || isNaN(start)) {
+      start = 0;
+    } else if (start > this.length) {
+      start = this.length;
+    }
+    if (end < 0 || isNaN(end)) {
+      end = 0;
+    } else if (end > this.length) {
+      end = this.length;
+    }
+    if (typeof this._value != 'undefined') {
+      return this._value.substring(start, end);
+    } else if (this._left && this._right) {
+      const leftLength = this._left.length;
+      const leftStart = Math.min(start, leftLength);
+      const leftEnd = Math.min(end, leftLength);
+      const rightLength = this._right.length;
+      const rightStart = Math.max(0, Math.min(start - leftLength, rightLength));
+      const rightEnd = Math.max(0, Math.min(end - leftLength, rightLength));
+      if (leftStart != leftEnd) {
+        if (rightStart != rightEnd) {
+          return this._left.substring(leftStart, leftEnd) + this._right.substring(rightStart, rightEnd);
+        } else {
+          return this._left.substring(leftStart, leftEnd);
+        }
+      } else {
+        if (rightStart != rightEnd) {      
+          return this._right.substring(rightStart, rightEnd);
+        } else {
+          return '';
+        }
+      }
+    }
+    return '';
+  }
+
+  public substr(start: number, length?: number): string {
+    let end;
+    if (start < 0) {
+      start = this.length + start;
+      if (start < 0) {
+        start = 0;
+      }
+    }
+    if (typeof length == 'undefined') {
+      end = this.length;
+    } else {
+      if (length < 0) {
+        length = 0;
+      }
+      end = start + length;
+    }
+    return this.substring(start, end);
+  }
+
+  public charAt(position: number): string {
+    return this.substring(position, position + 1);
+  }
+
+  public byteOffset(position: number): number {
+    if (typeof this._value != 'undefined') {
+      return byteLength(this._value.substring(0, position));
+    } else if (this._left && this._right) {
+      const leftLength = this._left.length;
+      if (position < leftLength) {
+        return this._left.byteOffset(position);
+      } else {
+        return this._left.bytes + this._right.byteOffset(position - leftLength);
+      }
+    }
+    return 0;
+  }
+
+  public charCodeAt(position: number): number {
+    return this.substring(position, position + 1).charCodeAt(0);
+  }
+}
+
+export default Rope;
